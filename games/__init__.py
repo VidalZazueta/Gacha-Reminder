a6b375f517e9769f3ae762ddcc95d0ee105c4c04
Vidal Zazueta <vzazuet26@gmail.com>
2026-04-20 22:46:49 -0700

    Removed debug mode for various user commands, added a new game "Genshin Impact" to return events for. Updated the documentation.txt file

--- games/__init__.py
+++ games/__init__.py
@@ -1,10 +1,12 @@
 from .wuwa import get_wuwa_events_async, WUWA_CONFIG
 from .zzz import get_zzz_events_async, ZZZ_CONFIG
+from .genshinimpact import get_genshinimpact_events_async, GENSHINIMPACT_CONFIG
 
 # Aggregated config — add new games here as you expand the project
 GAME_CONFIG = {
     "wuwa": WUWA_CONFIG,
     "zzz": ZZZ_CONFIG,
+    "genshinimpact": GENSHINIMPACT_CONFIG,
 }
 
-__all__ = ["get_wuwa_events_async", "get_zzz_events_async", "GAME_CONFIG", "WUWA_CONFIG", "ZZZ_CONFIG"]
+__all__ = [ "GAME_CONFIG", "get_wuwa_events_async", "WUWA_CONFIG", "get_zzz_events_async", "ZZZ_CONFIG", "get_genshinimpact_events_async", "GENSHINIMPACT_CONFIG"]
